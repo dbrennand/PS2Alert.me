64f170c246f198b67292d8a7d2b4fbbece141946
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-06-24 13:22:45 +0100

    feat: Add Service Worker event listener.

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,1 +1,10 @@
 // Service Worker
+self.addEventListener('push', (event) => {
+    // Convert event data to JSON
+    const data = event.data.json();
+    // Show push notification
+    self.registration.showNotification(data.title, {
+        body: data.body,
+        icon: '🚨',
+    });
+});
