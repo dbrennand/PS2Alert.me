8067b9d8a242cd411718cec19f74d0cdce613cea
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-06-24 10:48:05 +0100

    Fix(multiple): Add fixes. See below.
    Updated Node version to v16.4.0. Altered imports.
    
    Fixed routes to be async for lowdb.
    
    Fix lowdb removal of subscription.

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 // Import required libraries
-const express = require('express');
-const path = require('path');
-const lowdb = require('lowdb');
+import express from 'express';
+import path from 'path';
+import { Low, JSONFile } from 'lowdb'
+// Fix for __dirname: https://github.com/nodejs/help/issues/2907#issuecomment-757446568
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Setup database using lowdb
 // Creates db.json at backend/db.json
 const dbFile = path.join(__dirname, 'db.json');
-const adapter = new lowdb.JSONFile(dbFile);
-const db = new lowdb.Low(adapter);
+const adapter = new JSONFile(dbFile);
+const db = new Low(adapter);
 
 // Read data from database JSON file, this will set db.data content
 await db.read();
@@ -24,7 +27,7 @@ app.use(express.static(path.join(__dirname, '../frontend')));
 app.use(express.json());
 
 // /add-subscription API endpoint
-app.post('/add-subscription', function (req, res) {
+app.post('/add-subscription', async (req, res) => {
   console.log(`Subscribing ${req.body.endpoint} for push notifications.`);
   // Add subscription object to database
   db.data.subscriptions.push(req.body);
@@ -34,10 +37,10 @@ app.post('/add-subscription', function (req, res) {
 });
 
 // /remove-subscription API endpoint
-app.delete('/remove-subscription', function (req, res) {
+app.delete('/remove-subscription', async (req, res) => {
   console.log(`Unsubscribing ${req.body.endpoint} from push notifications.`);
   // Remove subscription object from the database
-  db.data.subscriptions.remove({ endpoint: req.body.endpoint });
+  db.data.subscriptions = db.data.subscriptions.filter(endpoint => endpoint === req.body.endpoint);
   await db.write();
   // Successfully deleted resource HTTP status code
   res.status(204);
