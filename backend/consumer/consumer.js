b7ec42ea2326d3f8a2467bbac1e79c0d8a0ede7e
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-06-29 12:38:11 +0100

    feat: Add progress on consumer.js

---
+++ backend/consumer/consumer.js
@@ -0,0 +1,103 @@
+// Import required libraries
+import amqp from 'amqplib';
+import webpush from 'web-push';
+// Import custom functions and model
+import database from './config/database';
+import webpush from './config/webpush';
+import Notify from './models/notifyModel';
+
+// Connect to MongoDB
+database();
+// Setup web push credentials
+webpush();
+
+// Get RabbitMQ credentials
+const rabbitmqUsername = process.env.DBUSERNAME;
+const rabbitmqPassword = process.env.DBPASSWORD;
+
+// Connect to RabbitMQ
+amqp.connect(`ampq://${rabbitmqUsername}:${rabbitmqPassword}@rabbitmq:5672`, function (error, connection) {
+    if (error) {
+        // Log error to console and exit
+        console.error(`An error occurred connecting to RabbitMQ: ${error}`);
+        process.exit(1);
+    };
+    // Create a new channel
+    connection.createChannel(function (error, channel) {
+        if (error) {
+            // Log error to console, close connection and exit
+            console.error(`An error occurred creating a new channel to RabbitMQ: ${error}`);
+            connection.close();
+            process.exit(1);
+        };
+        // Declare name of queue
+        var queue = 'MetagameEvent';
+
+        // Wait for the Queue to exist before consuming messages
+        channel.assertQueue(queue, {
+            durable: true
+        });
+        console.log(`Waiting for messages (MetagameEvents) from queue: ${queue}. To exit press CTRL+C`);
+        // Callback for when RabbitMQ pushes messages to the consumer
+        channel.consume(queue, function (message) {
+            console.log(`Recieved message: ${message.content}`)
+            // Parse MetagameEvent JSON
+            var MetagameEventJson = JSON.parse(message.content);
+            // Get server name from ID
+            var serverName = getServerName(MetagameEventJson.world_id);
+            // Get world (continent) name from ID
+            var continentName = getContinentName(MetagameEventJson.zone_id);
+            // Create push notification object
+            var pushNotification = {
+                title: `${serverName}: Alert started!`,
+                body: `On continent ${continentName}.`
+            };
+            // Get matching Notify documents from MongoDB
+            var matchingNotifyDocuments = getMatchingNotifyDocuments(MetagameEventJson.world_id);
+            // Iterate over matching documents, sending a push notification using the subscription data
+            matchingNotifyDocuments.forEach((notifyDocument) => {
+                // Send push notification
+                webpush.sendNotification(notifyDocument.subscription, JSON.stringify(pushNotification));
+            })
+        }, {
+            noAck: true
+        });
+    });
+});
+
+function getServerName(serverID) {
+    // Server IDs and names: https://ps2.fisu.pw/api/territory/
+    // Declare object containing server IDs and names
+    const serverInfo = {
+        '1': 'Connery',
+        '10': 'Miller',
+        '13': 'Cobalt',
+        '17': 'Emerald',
+        '40': 'SolTech',
+    };
+    return serverInfo[serverID];
+};
+
+function getContinentName(zoneID) {
+    // Zone (continent) IDs and names: https://ps2.fisu.pw/api/territory/
+    // Declare object containing zone (continent) IDs and names
+    const zoneInfo = {
+        '2': 'Indar',
+        '4': 'Hossin',
+        '6': 'Amerish',
+        '8': 'Esamir',
+    };
+    return zoneInfo[zoneID];
+};
+
+function getMatchingNotifyDocuments(serverID) {
+    // Create array to populate with matching Notify documents
+    var matchingNotifyDocuments = [];
+    // Get Notify documents that match the server ID
+    // Use cursor as a stream for scalability
+    Notify.
+        find({ servers: [serverID] }).
+        cursor().
+        on('data', function (notifyDocument) { matchingNotifyDocuments.push(notifyDocument); }).
+        on('end', () => { console.log('Finished finding matching documents.'); return matchingNotifyDocuments; });
+};
