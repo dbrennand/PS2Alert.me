c9d5f2e2002fe9b72d6cf5f61dc764d0a1694404
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-07-02 12:05:48 +0100

    feat: Add MongoDB init script.

---
+++ backend/config/mongo-init.sh
@@ -0,0 +1,14 @@
+set -e
+
+mongo <<EOF
+use $MONGO_INITDB_DATABASE
+
+db.createUser({
+  user: '$MONGO_INITDB_ROOT_USERNAME',
+  pwd: '$MONGO_INITDB_ROOT_PASSWORD',
+  roles: [{
+    role: 'readWrite',
+    db: '$MONGO_INITDB_DATABASE'
+  }]
+})
+EOF
