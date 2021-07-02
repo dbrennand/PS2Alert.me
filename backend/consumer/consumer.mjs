ebda03753b82653f1f9feeb21dd65c50e5923226
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-07-02 14:58:53 +0100

    refactor: Fix import.
    Add more logging. Remove async syntax.

--- backend/consumer/consumer.mjs
+++ backend/consumer/consumer.mjs
@@ -1,5 +1,5 @@
 // Import required libraries
-import amqp from 'amqplib';
+import amqp from 'amqplib/callback_api.js';
 import webpush from 'web-push';
 // Import custom functions and model
 import database from './config/database.mjs';
@@ -23,50 +23,51 @@ const rabbitmqUsername = process.env.RABBITMQUSERNAME;
 const rabbitmqPassword = process.env.RABBITMQPASSWORD;
 
 // Connect to RabbitMQ
-console.log('Connecting to RabbitMQ...')
-amqp.connect(`amqp://${rabbitmqUsername}:${rabbitmqPassword}@rabbitmq:5672`, async function (error, connection) {
+console.log('Connecting to RabbitMQ...');
+amqp.connect(`amqp://${rabbitmqUsername}:${rabbitmqPassword}@rabbitmq:5672`, function (error, connection) {
     if (error) {
         // Log error to console and exit
         console.error(`An error occurred connecting to RabbitMQ: ${error}`);
         process.exit(1);
     };
     // Create a new channel
-    await connection.createChannel(async function (error, channel) {
+    connection.createChannel(function (error, channel) {
         if (error) {
             // Log error to console, close connection and exit
             console.error(`An error occurred creating a new channel to RabbitMQ: ${error}`);
-            await connection.close();
+            connection.close();
             process.exit(1);
         };
         // Declare name of queue
         var queue = 'MetagameEvent';
 
         // Wait for the Queue to exist before consuming messages
-        await channel.assertQueue(queue, {
+        channel.assertQueue(queue, {
             durable: true
         });
         console.log(`Waiting for messages (MetagameEvents) from queue: ${queue}. To exit press CTRL+C`);
         // Callback for when RabbitMQ pushes messages to the consumer
-        await channel.consume(queue, async function (message) {
-            console.log(`Recieved message: ${message.content}`)
+        channel.consume(queue, function (message) {
+            console.log(`Recieved message: ${message.content}`);
             // Parse MetagameEvent JSON
             var metagameEventJson = JSON.parse(message.content);
+            console.log(`MetagameEvent JSON: ${metagameEventJson}`);
             // Get server (world) name and zone (continent) name from IDs
-            var serverContinentName = Promise.all([getServerName(metagameEventJson.world_id), getContinentName(metagameEventJson.zone_id)])
+            var serverContinentName = Promise.all([getServerName(metagameEventJson.world_id), getContinentName(metagameEventJson.zone_id)]);
             // Create push notification object
             var pushNotification = {
                 title: `${serverContinentName[0]}: Alert started!`,
                 body: `On continent ${serverContinentName[1]}.`
             };
             // Get matching Notify documents from MongoDB
-            Notify.find({ servers: serverID }, async function (error, notifyDocuments) {
+            Notify.find({ servers: metagameEventJson.world_id }, function (error, notifyDocuments) {
                 if (error) {
                     // Log error to console
                     console.error(`An error occurred finding Notify documents from MongoDB: ${error}`);
                     return;
                 }
                 // For debugging purposes, output matching documents
-                console.log(notifyDocuments);
+                console.log(`Matching documents: ${notifyDocuments}`);
                 // Successfully found matching documents, iterate over each document using the subscription data to send a push notification
                 notifyDocuments.forEach((notifyDocument) => {
                     // Send push notification
