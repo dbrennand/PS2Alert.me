ff336db3c8951bf97531a8ea7d73ddf4134f3176
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-07-03 10:31:38 +0100

    fix: Add greater validation for model.

--- backend/models/notifyModel.mjs
+++ backend/models/notifyModel.mjs
@@ -3,7 +3,12 @@ import mongoose from 'mongoose';
 // Define Schema for notify model
 
 const notifySchema = new mongoose.Schema({
-    servers: [String],
+    servers: {
+        type: [String],
+        // Connery, Miller, Cobalt, Emerald, SolTech
+        enum: ['1', '10', '13', '17', '40'],
+        required: [true, 'Invalid server.'],
+    },
     subscription: {
         endpoint: { type: String, unique: true, required: true },
         // Firefox doesn't provide this but some browsers (such as Edge) do
