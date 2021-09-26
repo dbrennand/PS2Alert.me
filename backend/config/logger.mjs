76a67c590e101ce01b899b09e015d6be631452b1
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-09-26 23:09:18 +0100

    fix: Ignore pid and hostname.

--- backend/config/logger.mjs
+++ backend/config/logger.mjs
@@ -4,7 +4,7 @@ import pino from 'pino';
 // Export Pino logger
 export default pino({
     // Ignore pid and hostname
-    ignore: 'pid,hostname',
+    base: undefined,
     // Set formatter so level label is shown instead of an integer
     formatters: {
         level: (label) => {
