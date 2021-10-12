11694205e38ee982f8f34d746e0d40505f5c621b
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-10-12 21:29:47 +0100

    feat: Add event listener for `pushsubscriptionchange`.

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,6 @@
 // Service Worker
+
+// Listener for push events
 self.addEventListener('push', (event) => {
     // Convert event data to JSON
     const data = event.data.json();
@@ -8,4 +10,18 @@ self.addEventListener('push', (event) => {
         vibrate: [200, 100, 200, 100, 200],
         icon: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/police-cars-revolving-light_1f6a8.png',
     });
+});
+
+// Listener for push subscription change
+self.addEventListener('pushsubscriptionchange', (event) => {
+    event.waitUntil(
+        fetch('/edit-subscription', {
+            method: 'PATCH',
+            body: JSON.stringify({
+                oldEndpoint: event.oldSubscription.endpoint,
+                newSubscription: event.newSubscription
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        })
+    );
 });
