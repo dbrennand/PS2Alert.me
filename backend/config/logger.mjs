f14e0d637500dc4b63b61120a70e14e01603205d
dbrennand <52419383+dbrennand@users.noreply.github.com>
2022-03-13 17:26:56 +0000

    refactor: format using prettier.

--- backend/config/logger.mjs
+++ backend/config/logger.mjs
@@ -1,14 +1,12 @@
-// Import required library
-import pino from 'pino';
+import pino from "pino";
 
-// Export Pino logger
 export default pino({
-    // Ignore pid and hostname
-    base: undefined,
-    // Set formatter so level label is shown instead of an integer
-    formatters: {
-        level: (label) => {
-            return { level: label };
-        }
-    }
-})
+  // Ignore pid and hostname
+  base: undefined,
+  // Set formatters so level label is shown instead of an integer
+  formatters: {
+    level: (label) => {
+      return { level: label };
+    },
+  },
+});
