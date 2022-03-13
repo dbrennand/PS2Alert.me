e7df08eeb3f813b596239676801940a791a4e6f4
dbrennand <52419383+dbrennand@users.noreply.github.com>
2022-03-13 17:24:25 +0000

    refactor: format using prettier.

--- backend/config/database.mjs
+++ backend/config/database.mjs
@@ -1,20 +1,21 @@
-import mongoose from 'mongoose';
-import logger from './logger.mjs';
+import mongoose from "mongoose";
+import logger from "./logger.mjs";
 
 const connectionUri = process.env.MONGO_CONNECTION_URI;
 
 // Connect to MongoDB
 export default async () => {
-    await mongoose.connect(connectionUri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        // https://mongoosejs.com/docs/5.x/docs/deprecations.html#findandmodify
-        useFindAndModify: false
-    }).then(
-        logger.info('Connected to MongoDB.')
-    ).catch((error) => {
-        logger.error(`Failed to connect to MongoDB: ${error}`);
-        process.exit(1);
+  await mongoose
+    .connect(connectionUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      // https://mongoosejs.com/docs/5.x/docs/deprecations.html#findandmodify
+      useFindAndModify: false,
     })
+    .then(logger.info("Connected to MongoDB."))
+    .catch((error) => {
+      logger.error(`Failed to connect to MongoDB: ${error}`);
+      process.exit(1);
+    });
 };
