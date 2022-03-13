defb28d54b094d5f1d587f790059e911ff0b1389
dbrennand <52419383+dbrennand@users.noreply.github.com>
2022-03-13 17:18:50 +0000

    refactor: General cleanup. Move to Promises.

--- backend/config/database.mjs
+++ backend/config/database.mjs
@@ -1,26 +1,20 @@
-// Import required library
 import mongoose from 'mongoose';
-// Custom import
 import logger from './logger.mjs';
 
-// Get MongoDB connection URI
 const connectionUri = process.env.MONGO_CONNECTION_URI;
 
-// Export function to connect to MongoDB
+// Connect to MongoDB
 export default async () => {
-    // Connect to MongoDB
-    try {
-        await mongoose.connect(connectionUri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            // https://mongoosejs.com/docs/5.x/docs/deprecations.html#findandmodify
-            useFindAndModify: false
-        });
-        logger.info('Connected to MongoDB.');
-    } catch (error) {
-        // Log error and exit
+    await mongoose.connect(connectionUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        // https://mongoosejs.com/docs/5.x/docs/deprecations.html#findandmodify
+        useFindAndModify: false
+    }).then(
+        logger.info('Connected to MongoDB.')
+    ).catch((error) => {
         logger.error(`Failed to connect to MongoDB: ${error}`);
         process.exit(1);
-    }
+    })
 };
