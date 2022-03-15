c4ecb5af5c3433f6b0a988fc95db88e8166583ad
dbrennand <52419383+dbrennand@users.noreply.github.com>
2022-03-15 18:28:55 +0000

    refactor: rewrite backend server to use promises.
    style: format using prettier.

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
-import pino from 'pino-http';
-import sanitize from 'mongo-sanitize';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import csurf from 'csurf';
-import helmet from 'helmet';
-import database from './config/database.mjs';
-import logger from './config/logger.mjs';
-import Notify from './models/notify.mjs';
+import pino from "pino-http";
+import sanitize from "mongo-sanitize";
+import express from "express";
+import cookieParser from "cookie-parser";
+import csurf from "csurf";
+import helmet from "helmet";
+import database from "./config/database.mjs";
+import logger from "./config/logger.mjs";
+import Notify from "./models/notify.mjs";
 // Fix for __dirname: https://github.com/nodejs/help/issues/2907#issuecomment-757446568
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
@@ -17,7 +17,7 @@ app.use(pino({ logger: logger, autoLogging: false, quietReqLogger: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 // Serve JS assets at /
-app.use('/', express.static(path.join(__dirname, '../frontend')));
+app.use("/", express.static(path.join(__dirname, "../frontend")));
 app.use(
   // https://github.com/helmetjs/helmet#reference
   helmet({
@@ -25,11 +25,23 @@ app.use(
     contentSecurityPolicy: {
       useDefaults: true,
       directives: {
-        'default-src': ["'self'"],
-        'script-src': ["'self'", 'https://ps2alert.me', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js', 'https://cdn.jsdelivr.net/npm/idb-keyval@6/dist/umd.js'],
-        'img-src': ["'self'", 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/'],
-        'style-src': ["'self'", 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css'],
-        'upgrade-insecure-requests': []
+        "default-src": ["'self'"],
+        "script-src": [
+          "'self'",
+          "https://ps2alert.me",
+          "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js",
+          "https://cdn.jsdelivr.net/npm/idb-keyval@6/dist/umd.js",
+        ],
+        "img-src": [
+          "'self'",
+          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/",
+        ],
+        "style-src": [
+          "'self'",
+          "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+          "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css",
+        ],
+        "upgrade-insecure-requests": [],
       },
     },
     // Disable and enable certain middlewares
@@ -39,31 +51,31 @@ app.use(
     expectCt: true,
     referrerPolicy: true,
     hsts: {
-      preload: false
+      preload: false,
     },
     noSniff: true,
     originAgentCluster: false,
     dnsPrefetchControl: {
-      allow: true
+      allow: true,
     },
     ieNoOpen: true,
     frameguard: true,
     permittedCrossDomainPolicies: true,
     hidePoweredBy: true,
-    xssFilter: true
+    xssFilter: true,
   })
 );
-app.set('views', path.join(__dirname, '../frontend/views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "../frontend/views"));
+app.set("view engine", "pug");
 
 const csrfProtection = csurf({
   // https://github.com/expressjs/csurf#cookie
   cookie: {
     // Sign the cookie using the COOKIE_SECRET when in production
-    signed: process.env.NODE_ENV === 'production' ? true : false,
+    signed: process.env.NODE_ENV === "production" ? true : false,
     // Set cookie as secure when production
     // Can only be sent over a HTTPS connection
-    secure: process.env.NODE_ENV === 'production' ? true : false,
+    secure: process.env.NODE_ENV === "production" ? true : false,
     // One day in seconds
     maxAge: 24 * 60 * 60,
     // Make the cookie inaccessible in the client's JavaScript
@@ -71,9 +83,9 @@ const csrfProtection = csurf({
     // Declare if cookies should be restricted to a first-party or same-site context
     // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
     // true when production, otherwise false
-    sameSite: process.env.NODE_ENV === 'production' ? true : false
-  }
-})
+    sameSite: process.env.NODE_ENV === "production" ? true : false,
+  },
+});
 
 /**
  * Sanitise input to mitigate against query selector injection attacks in NoSQL
@@ -82,89 +94,96 @@ const csrfProtection = csurf({
  */
 async function sanitiseBody(input) {
   return await sanitize(input);
-};
+}
 
 // PS2Alert.me routes
-app.get('/', csrfProtection, async (req, res) => {
-  res.render('index', { csrfToken: req.csrfToken() });
+app.get("/", csrfProtection, async (req, res) => {
+  res.render("index", { csrfToken: req.csrfToken() });
 });
 
-// /add-subscription API route
-app.post('/add-subscription', csrfProtection, async (req, res) => {
-  req.log.info(`Subscribing ${req.body.subscription.endpoint} for alert push notifications.`);
-  // Sanitise req.body to mitigate against query selector injection attacks
-  const cleanBody = await sanitiseInput(req.body);
-  // Create newNotify document from Notify model
-  const newNotify = new Notify(cleanBody);
-  // Save document to MongoDB
-  await newNotify.save({ validateBeforeSave: true, checkKeys: true }, async function (error, doc, _) {
-    if (error) {
-      // Log error and return HTTP error status code
-      req.log.error(`Failed to save Notify document to MongoDB: ${error}`);
-      res.sendStatus(500);
-      return;
-    }
-    req.log.info({
-      endpoint: doc.subscription.endpoint,
-      servers: doc.servers
-    }, 'Successfully added Notify document to MongoDB.');
-    // Successfully created resource HTTP status code
-    res.sendStatus(201);
+app.post("/api/post-subscription", csrfProtection, async (req, res) => {
+  req.log.info(
+    `Subscribing ${req.body.subscription.endpoint} for push notifications.`
+  );
+  sanitiseBody(req.body).then((cleanBody) => {
+    const newNotify = new Notify(cleanBody);
+    newNotify
+      .save({
+        validateBeforeSave: true,
+      })
+      .then((doc) => {
+        req.log.info(
+          {
+            endpoint: doc.subscription.endpoint,
+            servers: doc.servers,
+          },
+          "Successfully added Notify document to MongoDB."
+        );
+        res.sendStatus(201);
+      })
+      .catch((err) => {
+        req.log.error(`Failed to save Notify document to MongoDB: ${err}`);
+        res.sendStatus(500);
+      });
   });
 });
 
-// /remove-subscription API route
-app.delete('/remove-subscription', csrfProtection, async (req, res) => {
-  req.log.info(`Unsubscribing ${req.body.endpoint} from alert push notifications.`);
-  // Sanitise req.body.endpoint to mitigate against query selector injection attacks
-  const cleanEndpoint = await sanitiseInput(req.body.endpoint);
-  req.log.info(`Deleting Notify document matching the endpoint: ${cleanEndpoint}`);
-  // Remove Notify document containing the matching endpoint
-  await Notify.where().findOneAndDelete({
-    'subscription.endpoint': { $eq: cleanEndpoint }
-  }, { rawResult: false }, async function (error, doc) {
-    if (error) {
-      // Log error and return HTTP error status code
-      req.log.error(`Failed to delete Notify document matching the endpoint: ${cleanEndpoint} from MongoDB: ${error}`);
-      res.sendStatus(500);
-      return;
-    };
-    req.log.info(`Successfully deleted Notify document matching the endpoint: ${doc.subscription.endpoint} from MongoDB.`);
-    // Successfully deleted resource HTTP status code
-    res.sendStatus(200);
-  }
-  );
+app.delete("/api/delete-subscription", csrfProtection, async (req, res) => {
+  req.log.info(`Unsubscribing ${req.body.endpoint} from push notifications.`);
+  sanitiseBody(req.body.endpoint).then((cleanEndpoint) => {
+    Notify.where()
+      .findOneAndDelete(
+        {
+          "subscription.endpoint": { $eq: cleanEndpoint },
+        },
+        { rawResult: false }
+      )
+      .then((doc) => {
+        req.log.info(
+          `Successfully deleted Notify document matching endpoint: ${doc.subscription.endpoint} from MongoDB.`
+        );
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        req.log.error(
+          `Failed to delete Notify document matching endpoint: ${cleanEndpoint} from MongoDB: ${err}`
+        );
+        res.sendStatus(500);
+      });
+  });
 });
 
-// /update-subscription API route
-app.patch('/update-subscription', async (req, res) => {
-  req.log.info(res.body, 'Subscription update received.');
-  // Sanitise req.body to mitigate against query selector injection attacks
-  const cleanBody = await sanitiseInput(req.body);
-  req.log.info(`Updating Notify document matching the old endpoint: ${cleanBody.oldSubscription.endpoint}`);
-  // Update Notify document matching the old endpoint
-  await Notify.where().findOneAndUpdate({ 'subscription.endpoint': { $eq: cleanBody.oldSubscription.endpoint } },
-    { subscription: cleanBody.newSubscription }, { new: true, rawResult: false },
-    async function (error, doc) {
-      if (error) {
-        // Log error and return HTTP error status code
-        req.log.error(`Failed to update Notify document matching the old endpoint: ${cleanBody.oldSubscription.endpoint}: ${error}`);
+app.patch("/api/patch-subscription", async (req, res) => {
+  req.log.info(JSON.stringify(res.body), "Subscription update received.");
+  sanitiseBody(req.body).then((cleanBody) => {
+    Notify.where()
+      .findOneAndUpdate(
+        {
+          "subscription.endpoint": { $eq: cleanBody.oldSubscription.endpoint },
+        },
+        { subscription: cleanBody.newSubscription },
+        { new: true, rawResult: false }
+      )
+      .then((doc) => {
+        req.log.info(
+          {
+            endpoint: doc._doc.subscription.endpoint,
+            servers: doc._doc.servers,
+          },
+          `Successfully updated Notify document matching old endpoint: ${cleanBody.oldSubscription.endpoint}`
+        );
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        req.log.error(
+          `Failed to update Notify document matching old endpoint: ${cleanBody.oldSubscription.endpoint}: ${err}`
+        );
         res.sendStatus(500);
-        return;
-      };
-      req.log.info({
-        endpoint: doc._doc.subscription.endpoint,
-        servers: doc._doc.servers
-      },
-        `Successfully updated Notify document matching the old endpoint: ${cleanBody.oldSubscription.endpoint}`);
-      // Successfully updated resource HTTP status code
-      res.sendStatus(200);
-    }
-  );
+      });
+  });
 });
 
 // Connect to MongoDB
-database();
+await database();
 
-// Start the Express server
 app.listen(8080);
